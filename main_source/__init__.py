8edc3140a5189eeb69522672d29f7bbf3b484c75
Jovan Villaflores <jovan.villaflores@play-asia.com>
2023-03-17 17:19:01 +0800

    setup templates

--- main_source/__init__.py
+++ main_source/__init__.py
@@ -5,6 +5,10 @@ def create_app():
     app.config['SECRET_KEY'] = 'qwertyuiop'
 
     from .views import views
+    from .auth import auth
+
+    #register routes
     app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
