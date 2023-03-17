fcd95662912e958bcaafbc8f568d4d070d2b316c
Jovan Villaflores <jovan.villaflores@play-asia.com>
2023-03-17 16:18:48 +0800

    initial setup test

--- main_source/app.py
+++ main_source/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+##
+#from flask import Flask
 
-app = Flask(__name__)
+##app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World JJJ"
+##@app.route("/")
+##def home():
+## return "Hello World JJJ"
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+##if __name__ == '__main__':
+##  app.run(debug=True, port=8000)
